c03e5c0f1d9de8d63b3bba69dc4492d1a376b4b2
Xander Low <xander@xandydandy.com>
2023-04-04 16:51:57 -0700

    monorepo time

--- packages/chakra-theme/src/index.ts
+++ packages/chakra-theme/src/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import { colors } from "./colors";
-import { components } from "./components";
-import { fonts } from "./typography";
+import { components } from "../components";
+import { fonts, fontSizes } from "./typography";
 
 export const theme = extendTheme({
   colors,
   components,
   fonts,
+  fontSizes,
 });
