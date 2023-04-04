4ca8dfad538815a54fffb490b90df8e07a413450
Xander Low <xander@xandydandy.com>
2023-04-03 18:48:52 -0700

    updated typography

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import { colors } from "./colors";
+import { components } from "./components";
 import { fonts } from "./typography";
 
 export const theme = extendTheme({
   colors,
+  components,
   fonts,
 });
--- src/typography.ts
+++ src/typography.ts
@@ -3,3 +3,19 @@ export const fonts: Record<string, string> = {
   body: "DM Sans, sans-serif",
   caption: "Futura, sans-serif",
 };
+
+export const fontSizes: Record<string, string> = {
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "3xl": "1.9375rem",
+  "4xl": "2.375rem",
+  "5xl": "3rem",
+  "6xl": "3.5rem",
+  "7xl": "4rem",
+  "8xl": "6rem",
+  "9xl": "8rem",
+};
