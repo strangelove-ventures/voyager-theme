09930b7b12f4e010e0b62ebb69e8358f407ec879
Xander Low <xander@xandydandy.com>
2023-04-05 16:30:20 -0700

    updated: dir format

--- apps/next-app/src/pages/_app.tsx
+++ apps/next-app/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "@strangelovelabs/voyager-theme";
+import { GlobalFonts } from "@/styles/fonts";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
--- packages/chakra-theme/src/index.ts
+++ packages/chakra-theme/src/index.ts
@@ -1,7 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 
 import { colors } from "./colors";
-import { components } from "../components";
+import { components } from "./components";
 import { fonts, fontSizes } from "./typography";
 
 export const theme = extendTheme({
